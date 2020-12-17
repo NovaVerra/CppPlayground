4915b5cf35dc012d8a32ecf3ca1192105e74e387
lununglee <leo.lunung.lee@gmail.com>
2020-12-16 17:47:24 -0800

    slightly more advanced way of solving section challenge

--- section_08/section_challenge/srcs/main.cpp
+++ section_08/section_challenge/srcs/main.cpp
@@ -8,44 +8,40 @@ int	main()
 
 void	section_challenge()
 {
-	int	value_in_cents {0};
-	int	dollar {0};
-	int	quarter {0};
-	int	dime {0};
-	int	nickle {0};
-	int	penny {0};
+	const int	DOLLAR_VALUE {100};
+	const int	QUARTER_VALUE {25};
+	const int	DIME_VALUE {10};
+	const int	NICKLE_VALUE {5};
+	const int	PENNY_VALUE {1};
+
+	int			value_in_cents {0};
+	int			dollar {0};
+	int			quarter {0};
+	int			dime {0};
+	int			nickle {0};
+	int			penny {0};
 
 	cout << "Please enter the number of cents you have: ";
 	cin >> value_in_cents;
 	
-	while (value_in_cents >= 100)
-	{
-		value_in_cents -= 100;
-		dollar++;
-	}
-	while (value_in_cents >= 25)
-	{
-		value_in_cents -= 25;
-		quarter++;
-	}
-	while (value_in_cents >= 10)
-	{
-		value_in_cents -= 10;
-		dime++;
-	}
-	while (value_in_cents >= 5)
-	{
-		value_in_cents -= 5;
-		nickle++;
-	}
-	while (value_in_cents >= 1)
-	{
-		value_in_cents -= 1;
-		penny++;
-	}
-	cout << "Dollar: " << dollar << endl;
+	dollar = value_in_cents / DOLLAR_VALUE;
+	value_in_cents -= dollar * DOLLAR_VALUE;
+
+	quarter = value_in_cents / QUARTER_VALUE;
+	value_in_cents -= quarter * QUARTER_VALUE;
+
+	dime = value_in_cents / DIME_VALUE;
+	value_in_cents -= dime * DIME_VALUE;
+
+	nickle = value_in_cents / NICKLE_VALUE;
+	value_in_cents -= nickle * NICKLE_VALUE;
+
+	penny = value_in_cents / PENNY_VALUE;
+	value_in_cents -= penny * PENNY_VALUE;
+
+	cout << "Dollar:  " << dollar << endl;
 	cout << "Quarter: " << quarter << endl;
-	cout << "Dime: " << dime << endl;
-	cout << "Nickle: " << nickle << endl;
-	cout << "Penny: " << penny << endl;
+	cout << "Dime:    " << dime << endl;
+	cout << "Nickle:  " << nickle << endl;
+	cout << "Penny:   " << penny << endl;
 }
