7bbff42e2f3f16220273d658191555d65c169ca6
lununglee <leo.lunung.lee@gmail.com>
2021-01-22 15:09:04 -0800

    setup section challenge

---
+++ section_14/section_challenge/includes/section_challenge/MyString.h
@@ -0,0 +1,24 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::ostream;
+using std::istream;
+
+class	MyString
+{
+	// Constructors
+	// Destructors
+	// Mover
+	// Copier
+	// Basic getters
+	// Basic setters
+	// Overloaders
+};
+
+#endif
---
+++ section_14/section_challenge/srcs/main.cpp
@@ -0,0 +1,13 @@
+#include "../includes/section_challenge/header.h"
+#include "../includes/section_challenge/MyString.h"
+
+int	main()
+{
+
+	return 0;
+}
+
+void	section_challenge()
+{
+	
+}
