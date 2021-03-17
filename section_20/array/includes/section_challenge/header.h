8c31fcce38cc9e1627b114c0bb7ff50440f1d9a6
lununglee <leo.lunung.lee@gmail.com>
2021-03-16 20:23:10 -0700

    mid-way through array

---
+++ section_20/array/includes/section_challenge/header.h
@@ -0,0 +1,24 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <iostream>		// std::cin | std::cout
+#include <string>		// CPP-style string (objects)
+#include <vector>		// std::vector
+#include <iomanip>		// setw() | setprecision()
+#include <array>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::array;
+using std::sort;
+using std::min_element;
+using std::max_element;
+using std::adjacent_find;
+
+/* Insert Function Prototypes Here */
+
+#endif
---
+++ section_20/array/srcs/main.cpp
@@ -0,0 +1,121 @@
+#include "../includes/section_challenge/header.h"
+
+void	display(array<int, 5> &arr)
+{
+	cout << "[ ";
+	for (auto i : arr)
+		cout << i << " ";
+	cout << "]" << endl;
+}
+
+void	test_01()
+{
+	array<int, 5>	arr {1, 2, 3, 4, 5};
+	array<int, 5>	arr2 {};
+
+	display(arr);
+	display(arr2);
+
+	cout << "Array 1: " << "Size of array 1 is: " << arr.size() << endl;
+	cout << "Array 2: " << "Size of array 2 is: " << arr2.size() << endl;
+
+	arr2 = {10, 20, 30, 40, 50};
+	display(arr);
+	display(arr2);
+
+	arr[0] = 1000;
+	arr.at(1) = 2000;
+	display(arr);
+
+	cout << "Array 1: " << arr2.front() << endl;
+	cout << "Array 2: " << arr2.back() << endl;
+}
+
+void	test_02()
+{
+	array<int, 5>	arr {1, 2, 3, 4, 5};
+	array<int, 5>	arr2 {};
+
+	cout << "Array 1: " << "Size of array 1 is: " << arr.size() << endl;
+	cout << "Array 2: " << "Size of array 2 is: " << arr2.size() << endl;
+
+	display(arr);
+	display(arr2);
+
+	arr2.fill(9);
+
+	display(arr);
+	display(arr2);
+
+	arr.swap(arr2);
+
+	display(arr);
+	display(arr2);
+}
+
+void	test_03()
+{
+	array<int, 5>	arr {1, 2, 3, 4, 5};
+	int	*ptr = arr.data();
+	cout << *ptr + 3 << endl;
+	*ptr = 1000;
+	display(arr);
+}
+
+void	test_04()
+{
+	array<int, 5>	arr {4, 3, 2, 5, 1};
+	sort(arr.begin(), arr.end());
+	display(arr);
+
+}
+
+void	test_05()
+{
+	array<int, 5>	arr {1, 2, 3, 4, 5};
+	auto	min_it = min_element(arr.begin(), arr.end());
+	auto	max_it = max_element(arr.begin(), arr.end());
+	cout << *min_it << endl;
+	cout << *max_it << endl;
+}
+
+void	test_06()
+{
+	array<int, 5>	arr {1, 2, 3, 3, 5};
+	
+}
+
+void	test_07()
+{
+
+}
+
+void	test_08()
+{
+
+}
+
+void	test_09()
+{
+
+}
+
+void	test_10()
+{
+
+}
+
+int	main()
+{
+	// test_01();
+	// test_02();
+	// test_03();
+	// test_04();
+	test_05();
+	test_06();
+	test_07();
+	test_08();
+	test_09();
+	test_10();
+	return 0;
+}
